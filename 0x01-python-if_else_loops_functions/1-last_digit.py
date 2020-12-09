61d18835f87dff7c3829d7e0805a7ab71fbeaaa1
jnsoler140214 <josenico1212@gmail.com>
2020-12-08 22:17:14 -0500

    Fix code file 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,31 +1,18 @@
 #!/usr/bin/python3
 import random
-import sys
 
 number = random.randint(-10000, 10000)
-copynum = number
-sys.stdout.write("Last digit of %s " % number)
 
-if number < 0:
-    number = number * -1
-
-last_number = number % 10
-
-if copynum < 0:
-    if last_number is 0:
-        sys.stdout.write("is %s " % last_number)
-        sys.stdout.write("and is 0")
-    else:
-        last_number = last_number - (last_number * 2)
-        sys.stdout.write("is %s " % last_number)
-        sys.stdout.write("and is less than 6 and not 0")
-
+if (number >= 0):
+    last_number = number % 10
 else:
-    if last_number > 5:
-        sys.stdout.write("is %s " % last_number)
-        sys.stdout.write("and is greater than 5")
-    else:
-        sys.stdout.write("is %s " % last_number)
-        sys.stdout.write("and is less than 6 and not 0")
-print ('\n')
-exit(1)
+    last_number = ((number * -1) % 10) * -1
+
+if last_number > 5:
+    string = "and is grater than 5"
+elif last_number == 0:
+    string = "and is 0"
+else:
+    string = "and is less than 6 and not 0"
+
+print("Last digit of {:d} is {:d} {}".format(number, last_number,string))
