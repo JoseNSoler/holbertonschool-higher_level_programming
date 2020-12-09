1f09084f82ea8ffe5227c21c14dfccf67c5a585d
jnsoler140214 <josenico1212@gmail.com>
2020-12-08 22:19:57 -0500

    Fix code file 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,10 +9,10 @@ else:
     last_number = ((number * -1) % 10) * -1
 
 if last_number > 5:
-    string = "and is grater than 5"
+    string = "and is greater than 5"
 elif last_number == 0:
     string = "and is 0"
 else:
     string = "and is less than 6 and not 0"
 
-print("Last digit of {:d} is {:d} {}".format(number, last_number,string))
+print("Last digit of {:d} is {:d} {}".format(number, last_number, string))
