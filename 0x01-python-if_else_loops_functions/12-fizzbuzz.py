ca9806a112e7e9b0f38ebab44d1dbd4cadbacced
JoseNSoler <josenico1212@gmail.com>
2020-12-09 21:58:52 +0000

    Fix file 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for x in range(1,101):
+    for x in range(1, 101):
         if(x % 5 == 0) and (x % 3 == 0):
             y = "FizzBuzz"
         elif(x % 5 == 0):
