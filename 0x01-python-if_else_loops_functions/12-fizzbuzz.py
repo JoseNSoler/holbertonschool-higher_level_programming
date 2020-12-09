e648353c69a6ca8ab45f4fe9dd9d43396ef87b5e
JoseNSoler <josenico1212@gmail.com>
2020-12-09 21:57:26 +0000

    Upload file 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for x in range(1,101):
+        if(x % 5 == 0) and (x % 3 == 0):
+            y = "FizzBuzz"
+        elif(x % 5 == 0):
+            y = "Buzz"
+        elif(x % 3 == 0):
+            y = "Fizz"
+        else:
+            y = str(x)
+        print("{:s} ".format(y), end="")
