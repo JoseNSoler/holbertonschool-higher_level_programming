10608d4291813771924d881eaa5d0d6953c0a5cd
jnsoler140214 <josenico1212@gmail.com>
2020-12-09 23:55:03 -0500

    Upload file 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
 def remove_char_at(str, n):
-    n_len = len(str)
-    n_len = n_len + 1
+    nleno = len(str)
+    nleno = nleno + 1
 
-    for x in range(0, n_len):
-        if (n < 0) or (n > n_len):
+    for x in range(0, nleno):
+        if (n < 0) or (n > nleno):
             return(str)
         if (x == n):
             copy2 = str.replace(str[x], '')
