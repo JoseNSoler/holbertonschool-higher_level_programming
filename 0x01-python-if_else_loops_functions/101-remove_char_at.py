516cded038a328283783efdceeb10a2f46c1d004
jnsoler140214 <josenico1212@gmail.com>
2020-12-09 23:55:48 -0500

    Fix spaces file 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
 def remove_char_at(str, n):
     nleno = len(str)
     nleno = nleno + 1
-
     for x in range(0, nleno):
         if (n < 0) or (n > nleno):
             return(str)
