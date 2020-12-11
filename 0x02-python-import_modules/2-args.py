a81ebcefe9bb2831c8e5cb36a6c561158ebaec72
jnsoler140214 <josenico1212@gmail.com>
2020-12-10 21:25:20 -0500

    Fix include mainString file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-cadena = str(sys.argv)
-count = len(sys.argv)
+if __name__ == "__main__":
+    cadena = str(sys.argv)
+    count = len(sys.argv)
 
-if (count <= 1):
-    print("0 arguments.")
-else:
-    minus_c = count - 1
-    print("{:d} arguments:".format(minus_c))
-    for x in range(1, count):
-        y = x
-        print("{:d}: ".format(y), end='')
-        print("{:s}".format(sys.argv[x]))
+    if (count <= 1):
+        print("0 arguments.")
+    else:
+        minus_c = count - 1
+        print("{:d} arguments:".format(minus_c))
+        for x in range(1, count):
+            y = x
+            print("{:d}: ".format(y), end='')
+            print("{:s}".format(sys.argv[x]))
