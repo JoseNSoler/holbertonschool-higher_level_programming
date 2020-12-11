141aaad41d4e416981dff3921a2d80ad3b0b9953
JoseNSoler <josenico1212@gmail.com>
2020-12-11 20:31:56 +0000

    Fix PEP8 file 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,17 +10,21 @@ if __name__ == "__main__":
         exit(1)
 
     sign = sys.argv[2]
-    num1, num2, switch = int(string[1]), int(string[3]), 0
+    num1, num2, result = int(string[1]), int(string[3]), 0
 
     if(leno == 3):
         if (sign == '+'):
-            print("{:d} + {:d} = {:d}".format(num1, num2, calculator_1.add(num1, num2)))
+            result = calculator_1.add(num1, num2)
+            print("{:d} + {:d} = {:d}".format(num1, num2, result))
         elif (sign == '-'):
-            print("{:d} + {:d} = {:d}".format(num1, num2, calculator_1.sub(num1, num2)))
+            result = calculator_1.sub(num1, num2)
+            print("{:d} + {:d} = {:d}".format(num1, num2, result))
         elif (sign == '*'):
-            print("{:d} + {:d} = {:d}".format(num1, num2, calculator_1.mul(num1, num2)))
+            result = calculator_1.mul(num1, num2)
+            print("{:d} + {:d} = {:d}".format(num1, num2, result))
         elif (sign == '/'):
-            print("{:d} + {:d} = {:d}".format(num1, num2, calculator_1.div(num1, num2)))
+            result = calculator_1.div(num1, num2)
+            print("{:d} + {:d} = {:d}".format(num1, num2, result))
         else:
             print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
