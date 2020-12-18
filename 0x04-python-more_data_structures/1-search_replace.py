a862b8b56b511642dd71a90982e0391a95fadd25
jnsoler140214 <josenico1212@gmail.com>
2020-12-17 22:49:35 -0500

    First try Upload files 3 2 1 let's jam

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,5 +6,5 @@ def search_replace(my_list, search, replace):
     cop_list = my_list[:]
     for x in range(len(cop_list)):
         if cop_list[x] == search:
-            new_list[x] = replace
+            cop_list[x] = replace
 return (cop_list)
