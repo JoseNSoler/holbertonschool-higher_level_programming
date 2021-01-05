348dd6fc6dfb6d2e6dfdee45b063cfa63cb389aa
JoseNSoler <josenico1212@gmail.com>
2021-01-05 04:27:07 +0000

    Fix PEP8 file 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -3,8 +3,8 @@
 
 def safe_print_division(a, b):
 
-    """ 
-    Function that divides 2 integers 
+    """
+    Function that divides 2 integers
     and prints the result
     Args:
         a: number 1
