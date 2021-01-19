3cb3a33cf7836432afb1be91cc7df21368ef78e3
JoseNSoler <josenico1212@gmail.com>
2021-01-19 15:13:07 +0000

    UPLOAD file and README.md

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module - list of available attributes and methods of an object'''
+
+
+def lookup(obj):
+    '''Returns available attributes and methods of an object'''
+    return dir(obj)
