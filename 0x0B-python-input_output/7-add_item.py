ceaae455d5c70bbe90e17aaf82c6382fd5729b4b
JoseNSoler <josenico1212@gmail.com>
2021-01-28 19:29:08 +0000

    FIX Pep8 file 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,7 +7,6 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-
 def load_add_save():
     '''
     Returns a object based on a JSON file
