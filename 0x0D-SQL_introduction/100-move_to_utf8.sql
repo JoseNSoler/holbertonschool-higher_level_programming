8cfa37c5c339d806376f282086ca5a54b924ed83
JoseNSoler <josenico1212@gmail.com>
2021-03-02 06:46:46 +0000

    FIX comments file 100

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,15 @@
 -- convert all of the following to UTF8 in database `first_table`.
 
+-- User of database.
 USE `hbtn_0c_0`;
 
--- For each database:
+-- For each database.
 ALTER DATABASE
     `hbtn_0c_0`
     CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_unicode_ci;
 
--- For each table
+-- For each table.
 ALTER TABLE
     `first_table`
     MODIFY COLUMN `name`
