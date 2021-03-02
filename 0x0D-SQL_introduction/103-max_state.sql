2449281c1790bcac6bb4667bf32818100c61e6b4
JoseNSoler <josenico1212@gmail.com>
2021-03-02 09:00:02 +0000

    UPLOAD file 103

---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,9 @@
+-- List average temperature of city in table `temperatures`.
+-- Use of database.
+USE `hbtn_0c_0`;
+
+-- Average temperature of cities.
+SELECT `State`, MAX(`value`) AS `max_temp`
+FROM `temperatures`
+GROUP BY `State`
+ORDER BY `State` ASC;
