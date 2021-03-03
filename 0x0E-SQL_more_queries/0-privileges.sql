43db156745e1341add29c6a92a92096ff5fceccd
JoseNSoler <josenico1212@gmail.com>
2021-03-03 06:08:30 +0000

    FIX format code file 0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Shows priviligies for users 1 - 2.
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
