8c515292b6afec8c3b66186a1ac53e3e7b63f406
JoseNSoler <josenico1212@gmail.com>
2021-03-24 06:54:05 +0000

    [FIX] logic error file 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
 
-if (process.argv[2] === undefined) console.log('No Arguments');
-
-if (process.argv[2] !== undefined && process.argv[3] === undefined) {
-  console.log('Argument found');
+if (process.argv[2] === undefined) {
+  console.log('No Arguments');
 } else {
-  console.log('Arguments found');
+  if (process.argv[2] !== undefined && process.argv[3] === undefined) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
 }
