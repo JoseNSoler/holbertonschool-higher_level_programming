780a159f12460ae2f8fbdc58b4ed81b5fd769d2b
JoseNSoler <josenico1212@gmail.com>
2021-03-24 06:56:33 +0000

    [FIX] syntax error file 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-
 if (process.argv[2] === undefined) {
-  console.log('No Arguments');
+  console.log('No argument');
 } else {
   if (process.argv[2] !== undefined && process.argv[3] === undefined) {
     console.log('Argument found');
