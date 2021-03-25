79b599024aa8f6d261c7c57edbca8a2a4d19a3a9
JoseNSoler <josenico1212@gmail.com>
2021-03-25 03:36:11 +0000

    [FIX] file logic 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,16 @@
 
 const copyArgv = process.argv.slice(2).sort(function (a, b) { return b - a; });
 const empty = Boolean(isNaN(parseInt(process.argv[2])) ||
-                      process.argv[2] === undefined ||
-                      parseInt(process.argv[2]) === 1);
+                      process.argv[2] === undefined);
 
 if (empty) {
   console.log(0);
 } else {
   const maxNum = copyArgv[0];
+  if (maxNum === '1') {
+    console.log(0);
+    return process.exit(1);
+  }
   copyArgv.forEach((val, index) => {
     if (val !== maxNum) {
       console.log(val);
